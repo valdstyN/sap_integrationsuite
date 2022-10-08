c5241a926eb90396e07aceeb424cae9ce1839eaa
valdstyN <valdstyn@gmail.com>
2022-10-08 15:37:35 +0200

    Add files via upload

---
+++ log_attachment.groovy
@@ -0,0 +1,11 @@
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+
+def Message processData(Message message) {
+	def body = message.getBody(java.lang.String) as String;
+	def messageLog = messageLogFactory.getMessageLog(message);
+	if(messageLog != null){
+		messageLog.addAttachmentAsString("Execution Log", logMessage, "text/plain");
+	}
+	return message;
+}
